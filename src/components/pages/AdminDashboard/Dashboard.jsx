e114483fb1cc0d0eb049b7508a59e63e3bc952ed
Agustín Dominguez <131421006+AgusDomz@users.noreply.github.com>
2023-09-20 20:24:24 -0300

    dashborad tables added

--- src/components/pages/AdminDashboard/Dashboard.jsx
+++ src/components/pages/AdminDashboard/Dashboard.jsx
@@ -1,45 +1,14 @@
-import "./Dashboard.css"
-import { Link } from 'react-router-dom';
-import Axios from 'axios'
+import MenuTable from "../AdminDashboard/Tables/MenuTable";
+import UserTable from "../AdminDashboard/Tables/UserTable";
+import OrdersTable from "../AdminDashboard/Tables/OrderTable";
 
 function Dashboard() {
-  
   return (
-    <div>
-      <Link className="btn btn-primary mt-2" to="">+add menu</Link>
-      <table className="table text-center mt-4 border">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">State</th>
-            <th scope="col">Detail</th>
-            <th scope="col">Category</th>
-            <th scope="col">Update</th>
-            <th scope="col">Delete</th>
-            <th scope="col">Change State</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">Coffee</th>
-            <th>$13</th>
-            <th>in-stock</th>
-            <th>Cup of coffee</th>
-            <th>cold-drink</th>
-            <th>
-              <Link className="btn btn-warning">Update</Link>
-            </th>
-            <th>
-              <Link className="btn btn-danger">Delete</Link>
-            </th>
-            <th>
-              <Link className="btn btn-success">Change</Link>
-            </th>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      <MenuTable />
+      <UserTable />
+      <OrdersTable />
+    </>
   )
 }
 
